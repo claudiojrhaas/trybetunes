c18c1aaef007523f8e5d16ca76b70c1448bd4590
Cláudio Haas <claudiohaas.cjh@gmail.com>
2022-05-23 22:38:43 -0300

    Req 2 | Criado form e estado do componente

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    nameUser: '',
+    validateBtnLogin: false,
+  }
+
+  validateBtnLogin = () => {
+    const { userName, validateBtnLogin } = this.state;}
+
+    nameUser.length >= 3 ? this.setState({ validateBtnLogin: true }) : this.setState({validateBtnLogin: false })
+  }
+
   render() {
+    const { nameUser, validateBtnLogin } = this.state;
+
     return (
       <div data-testid="page-login">
-        <h1>Login</h1>
+        <Form>Login
+          <label htmlFor="login-name">
+            <input
+              id="login-name"
+              type="text"
+              placeholder="name"
+              data-testid="login-name-input"
+              value={ nameUser }
+              onChange="?"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="login-submit-button"
+            disabled={ validateBtnLogin }
+            onSubmit="?"
+          >
+            Entrar
+          
+          </button>
+        </Form>
       </div>
     );
   }
