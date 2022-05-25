c56a14a951968d02c53c8eda799bd785ea24e0b4
Cláudio Haas <claudiohaas.cjh@gmail.com>
2022-05-25 19:29:26 -0300

    Concluído: Req 4 | Criado Links para o Header

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header
+        data-testid="header-component"
+        className="header-component"
+      >
+        Cabeçalho
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+      </header>
+    );
+  }
+}
+
+export default Header;
